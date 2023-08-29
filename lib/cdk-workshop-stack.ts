cbad67d2c66f91a992920695c5d1686bf60a4a3b
Harrison Oliveira <harrisonlopeslb@gmail.com>
2023-08-29 18:47:41 -0300

    feat: clear CdkWorkshopStack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,8 @@
 import * as cdk from 'aws-cdk-lib'
 import { type Construct } from 'constructs'
-import * as sqs from 'aws-cdk-lib/aws-sqs'
-import * as sns from 'aws-cdk-lib/aws-sns'
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor (scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    })
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic')
-
-    topic.addSubscription(new subs.SqsSubscription(queue))
   }
 }
--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,15 +1,1 @@
-import { CdkWorkshopStack } from '@lib/cdk-workshop-stack'
-import * as cdk from 'aws-cdk-lib'
-import { Template } from 'aws-cdk-lib/assertions'
-
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App()
-  const stack = new CdkWorkshopStack(app, 'MyTestStack')
-
-  const template = Template.fromStack(stack)
-
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  })
-  template.resourceCountIs('AWS::SNS::Topic', 1)
-})
+test('', () => {})
