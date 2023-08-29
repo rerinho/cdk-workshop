cd1883da4225e22d58271cbfe373ee0c2c8ab694
Harrison Oliveira <harrisonlopeslb@gmail.com>
2023-08-29 11:34:01 -0300

    style: apply eslint

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack'
 
-const app = new cdk.App();
+const app = new cdk.App()
 new CdkWorkshopStack(app, 'CdkWorkshopStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -18,4 +18,4 @@ new CdkWorkshopStack(app, 'CdkWorkshopStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+})
