aa46b6a0e4f1f0a07e1cdab08598eb7187b8c785
Harrison Oliveira <harrisonlopeslb@gmail.com>
2023-08-30 15:16:06 -0300

    feat: ensure Hello Lambda is initialized with correct properties

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,17 @@
 import * as cdk from 'aws-cdk-lib'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { type Construct } from 'constructs'
+import * as path from 'path'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor (scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    new NodejsFunction(this, 'HelloHandler', {
+      entry: path.join(__dirname, '../lambda/hello.ts'),
+      runtime: Runtime.NODEJS_16_X,
+      handler: 'hello'
+    })
   }
 }
--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,1 +1,17 @@
-test('', () => {})
+import { CdkWorkshopStack } from '@lib/cdk-workshop-stack'
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+
+describe('CdkWorkshopStack', () => {
+  test('should initialize Hello Lambda Function with correct properties', () => {
+    const app = new cdk.App()
+    const stack = new CdkWorkshopStack(app, 'CdkWorkshopStack')
+    const template = Template.fromStack(stack)
+
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Handler: 'index.hello',
+      Runtime: Runtime.NODEJS_16_X.name
+    })
+  })
+})
