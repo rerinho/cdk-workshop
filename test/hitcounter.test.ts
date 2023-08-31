54e32c66d2ad33e458945b2f4077128031ac95db
Harrison Oliveira <harrisonlopeslb@gmail.com>
2023-08-31 16:20:19 -0300

    refactor: remove code duplication and improve readability

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -5,32 +5,13 @@ import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 describe('HitCounter', () => {
   it('should initialize a DyanmoDB::Table', () => {
-    const stack = new cdk.Stack()
-    const fakeLambda = new lambda.Function(stack, 'FakeLambda', {
-      code: lambda.Code.fromInline('exports.handler = async function(event, context) {}'),
-      handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_16_X
-    })
-    new HitCounter(stack, 'HitCounter', {
-      downstream: fakeLambda
-    })
-    const template = Template.fromStack(stack)
+    const template = makeStackTemplate()
 
     template.resourceCountIs('AWS::DynamoDB::Table', 1)
   })
 
   it('should iniatilize a HitCounter LambdaFunction with correct values', () => {
-    const stack = new cdk.Stack()
-    const fakeLambda = new lambda.Function(stack, 'FakeLambda', {
-      code: lambda.Code.fromInline('exports.handler = async function (event, context) {}'),
-      handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_16_X
-    })
-    new HitCounter(stack, 'HitCounter', {
-      downstream: fakeLambda
-    })
-    const template = Template.fromStack(stack)
-
+    const template = makeStackTemplate()
     template.hasResourceProperties('AWS::Lambda::Function', {
       Handler: 'index.hitCounter',
       Environment: {
@@ -46,3 +27,21 @@ describe('HitCounter', () => {
     })
   })
 })
+
+const makeFakeLambda = (stack: cdk.Stack): lambda.Function => {
+  const fakeLambda = new lambda.Function(stack, 'FakeLambda', {
+    code: lambda.Code.fromInline('exports.handler = async function (event, context) {}'),
+    handler: 'handler',
+    runtime: lambda.Runtime.NODEJS_16_X
+  })
+  return fakeLambda
+}
+
+const makeStackTemplate = (): Template => {
+  const stack = new cdk.Stack()
+  const lambda = makeFakeLambda(stack)
+  new HitCounter(stack, 'HitCounter', {
+    downstream: lambda
+  })
+  return Template.fromStack(stack)
+}
